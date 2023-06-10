57698062cf1df1ec193f8835d82c182ecfd08fe6
Yuli Tamara <79260463+Yulitamara@users.noreply.github.com>
2023-06-10 14:04:59 +0300

    nav

--- src/components/nabvar.jsx
+++ src/components/nabvar.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import "./navbar.css";
-// import buy from "../assets/gif.gif";
+import navImg from "../assets/nav-img.png";
 
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/cart">
+        <Link className="link" to="/cart">
           <ShoppingCart size={32} className="shoppingcart" />
         </Link>
-        {/* <Link to="/">
-          <img src={buy} alt="" />
-        </Link> */}
+        <Link className="link" to="/">
+          <img src={navImg} alt="" />
+        </Link>
       </div>
     </div>
   );
